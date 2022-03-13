3a46e073495c79589bc019a10c88de48875f550f
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-03-13 17:07:11 -0600

    Add files via upload

--- Process.py
+++ Process.py
@@ -1,12 +1,11 @@
-
 #realizar los imports
 import simpy
 import random
-
+import time
 #proceso del programa
-def programa(name,env,CPU, RAM,RunTimer):
+def programa(name,env,CPU, RAM,arrival,cores):
     #Tiempo de espera
-    yield env.timeout(RunTimer)
+    yield env.timeout(arrival)
     #tiempo de llegada
     Start = env.now
     print(f'{name} inicia en {Start}')
@@ -22,7 +21,7 @@ def programa(name,env,CPU, RAM,RunTimer):
         print(f'{name} tiene {instructions} tareas oara realizar')
         with CPU.request() as resource:
             yield resource #solicitar espacio de procesador
-            instructions -= 2 #se asume que puede hacer 2 instrucciones a la vez
+            instructions -= 2*cores #se asume que puede hacer 2 instrucciones a la vez por nucleo
             yield env.timeout(Timetoact) #simular tiempo de realizacion del proceso
     yield RAM.put(Resource) #regresar la memoria usada
     global tiempo_total
@@ -32,12 +31,21 @@ def programa(name,env,CPU, RAM,RunTimer):
     
 random.seed(10)
 env = simpy.Environment()  # crear ambiente de simulacion
-RAM = simpy.Container(env, 30, init=30)  # crea el container de la ram
-CPU = simpy.Resource(env, capacity=1)  # se crea el procesador con capacidad establecida
+RAM = simpy.Container(env, 100, init=100)  # crea el container de la ram
+CPU = simpy.Resource(env, capacity=2)  # se crea el procesador con capacidad establecida
 procesos = 50  # cantidad de procesos a generar
 tiempo_total = 0
+'''
+#multiples intervalos con cantidad de carga
+while x < 40: #40 intervalos de a 5, es decir 200 procesos
+    for i in range(5): #intervalos de a 5
+        env.process(programa(f'Programa {i}', env ,CPU, RAM,0,8))
+    time.sleep(0.05) #esperar 0.05 segundos por intervalo    
+'''
+
+#intervalo de solo 1
 for i in range(procesos):
-    env.process(programa(f'Programa {i}', env ,CPU, RAM,0))
+    env.process(programa(f'Programa {i}', env ,CPU, RAM,0,8))
 env.run()
 print(f'tiempo total {tiempo_total}')
 print(f'tiempo promedio {(tiempo_total/procesos)}')
