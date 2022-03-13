18e4c10e1666761f42d0e054e5303cc96006bcc0
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-03-13 16:53:49 -0600

    Add files via upload

--- Process.py
+++ Process.py
@@ -19,16 +19,28 @@ def programa(name,env,CPU, RAM,RunTimer):
     yield RAM.get(Resource) #esperar hasta haber cantidad de memoria disponible
     while instructions > 0:
         #iniciar proceso, si hay mas procesos antes, debe de esperar
-        print(f'{name} tiene {Instructions} tareas oara realizar')
+        print(f'{name} tiene {instructions} tareas oara realizar')
         with CPU.request() as resource:
             yield resource #solicitar espacio de procesador
-            instructions -= 2*req #se asume que puede hacer 2 instrucciones por nucleo
-            yield env.timout(Timetoact) #simular tiempo de realizacion del proceso
+            instructions -= 2 #se asume que puede hacer 2 instrucciones a la vez
+            yield env.timeout(Timetoact) #simular tiempo de realizacion del proceso
     yield RAM.put(Resource) #regresar la memoria usada
+    global tiempo_total
+    tiempo_total += (env.now) - Start
     print (f'{name} ha sido completado, tiempo de uso {(env.now) - Start}')
     
-random.seed(10)            
     
+random.seed(10)
+env = simpy.Environment()  # crear ambiente de simulacion
+RAM = simpy.Container(env, 30, init=30)  # crea el container de la ram
+CPU = simpy.Resource(env, capacity=1)  # se crea el procesador con capacidad establecida
+procesos = 50  # cantidad de procesos a generar
+tiempo_total = 0
+for i in range(procesos):
+    env.process(programa(f'Programa {i}', env ,CPU, RAM,0))
+env.run()
+print(f'tiempo total {tiempo_total}')
+print(f'tiempo promedio {(tiempo_total/procesos)}')
     
     
 '''
