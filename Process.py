93d9ccfc4bf54b3428648d8253b84ed8c0a8177e
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-03-14 15:54:58 -0600

    Add files via upload

--- Process.py
+++ Process.py
@@ -8,7 +8,7 @@ def programa(name,env,CPU, RAM,arrival,cores):
     yield env.timeout(arrival)
     #tiempo de llegada
     Start = env.now
-    print(f'{name} inicia en {Start}')
+    #print(f'{name} inicia en {Start}')
     
     #simular cantidad de tiempo requerido 
     Timetoact = random.randint(1, 10)
@@ -18,7 +18,7 @@ def programa(name,env,CPU, RAM,arrival,cores):
     yield RAM.get(Resource) #esperar hasta haber cantidad de memoria disponible
     while instructions > 0:
         #iniciar proceso, si hay mas procesos antes, debe de esperar
-        print(f'{name} tiene {instructions} tareas oara realizar')
+        #print(f'{name} tiene {instructions} tareas oara realizar')
         with CPU.request() as resource:
             yield resource #solicitar espacio de procesador
             instructions -= 2*cores #se asume que puede hacer 2 instrucciones a la vez por nucleo
@@ -26,26 +26,28 @@ def programa(name,env,CPU, RAM,arrival,cores):
     yield RAM.put(Resource) #regresar la memoria usada
     global tiempo_total
     tiempo_total += (env.now) - Start
-    print (f'{name} ha sido completado, tiempo de uso {(env.now) - Start}')
+    #print (f'{name} ha sido completado, tiempo de uso {(env.now) - Start}')
     
     
 random.seed(10)
 env = simpy.Environment()  # crear ambiente de simulacion
 RAM = simpy.Container(env, 100, init=100)  # crea el container de la ram
-CPU = simpy.Resource(env, capacity=2)  # se crea el procesador con capacidad establecida
-procesos = 50  # cantidad de procesos a generar
+CPU = simpy.Resource(env, capacity=1)  # se crea el procesador con capacidad establecida
+procesos = 100 # cantidad de procesos a generar
 tiempo_total = 0
-'''
-#multiples intervalos con cantidad de carga
-while x < 40: #40 intervalos de a 5, es decir 200 procesos
-    for i in range(5): #intervalos de a 5
-        env.process(programa(f'Programa {i}', env ,CPU, RAM,0,8))
-    time.sleep(0.05) #esperar 0.05 segundos por intervalo    
-'''
+
+# #multiples intervalos con cantidad de carga
+# x = 0
+# while x < 40: #40 intervalos de a 5, es decir 200 procesos
+#     for i in range(5): #intervalos de a 5
+#         env.process(programa(f'Programa {i}', env ,CPU, RAM,0,8))
+#     time.sleep(0.05) #esperar 0.05 segundos por intervalo
+#     x+=1
+
 
 #intervalo de solo 1
 for i in range(procesos):
-    env.process(programa(f'Programa {i}', env ,CPU, RAM,0,8))
+    env.process(programa(f'Programa {i}', env ,CPU, RAM,0,3))
 env.run()
 print(f'tiempo total {tiempo_total}')
 print(f'tiempo promedio {(tiempo_total/procesos)}')
